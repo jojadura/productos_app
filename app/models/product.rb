55e0abf8c92daba8bf5009feb3a68fa56b0bda44
john j <as@gmail.com>
2015-12-21 14:38:57 -0500

    Listar y adicionar Productos

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,4 +10,5 @@
 #
 
 class Product < ActiveRecord::Base
+	has_and_belongs_to_many :categories
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151215021228) do
+ActiveRecord::Schema.define(version: 20151216234106) do
 
   create_table "categories", force: :cascade do |t|
     t.text     "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "categories_products", id: false, force: :cascade do |t|
+    t.integer "category_id", null: false
+    t.integer "product_id",  null: false
   end
 
   create_table "products", force: :cascade do |t|
