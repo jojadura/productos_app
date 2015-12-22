87a26e552792e9472a42be9cb6d9f33873232735
john j <as@gmail.com>
2015-12-22 08:10:12 -0500

    Editar y Eliminar Productos

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -21,8 +21,34 @@ class ProductsController < ApplicationController
     end     
   end
 
+  def edit
+    @product = Product.find(params[:id])    
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.categories.delete_all
+    params[:product][:category_ids].each do |category_id|
+        category = Category.find(category_id)
+        @product.categories.push(category)
+    end
+      
+    if @product.update(product_params)
+        redirect_to products_path
+    else
+        render :edit
+    end
+  end
+        
+
+  def destroy
+    product = Product.find(params[:id])
+    product.destroy
+    redirect_to products_path
+  end
+
   private 
     def product_params
-      params.require(:product).permit(:name, :price, category_ids:[])      
+      params.require(:product).permit(:name, :price, :category_ids)      
     end
 end
